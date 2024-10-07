5f181c4797c32dd29441e508c52056c3ba8a0d01
hongzio <hongzio@user.noreply.github.com>
2024-10-08 02:02:41 +0900

    feat: add git conflict key mappings to which-key
    - Added key mappings for git conflict resolution to which-key plugin.
    - Mappings include ours, theirs, none, both, next, and prev.
    - Added commands for listing and refreshing conflicts.
    - Updated which-key group for conflict resolution.

--- lazyvim/lua/plugins/which-key.lua
+++ lazyvim/lua/plugins/which-key.lua
@@ -3,7 +3,8 @@ return {
     "folke/which-key.nvim",
     opts = {
       spec = {
-        { "<leader>U", group = "+Undotree" },
+        { "<leader>U", group = "+undotree" },
+        { "<leader>h", group = "+conflict" },
       },
     },
   },
