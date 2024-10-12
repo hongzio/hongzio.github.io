bac425d6c22b040dce4c8e944d52887be2bf0a46
hongzio <hongzio@user.noreply.github.com>
2024-10-12 21:12:24 +0900

    feat: enhance nvim-cmp configuration
    - Changed `<CR>` mapping to use `cmp.mapping` for better integration.
    - Added `copilot` source with high priority and group index.
    - Reordered `codeium` source with high priority and group index.
    - Included `emoji` source for additional completion options.

--- lazyvim/lua/plugins/nvim-cmp.lua
+++ lazyvim/lua/plugins/nvim-cmp.lua
@@ -6,12 +6,22 @@ return {
     local cmp = require("cmp")
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-      ["<CR>"] = function(fallback)
+      ["<CR>"] = cmp.mapping(function(fallback)
         cmp.abort()
         fallback()
-      end,
+      end, {"i", "s"})
     })
+
     table.insert(opts.sources, { name = "emoji" })
-    table.insert(opts.sources, { name = "codeium" })
+    table.insert(opts.sources, 1, {
+      name = "copilot",
+      group_index = 1,
+      priority = 100,
+    })
+    table.insert(opts.sources, 1, {
+      name = "codeium",
+      group_index = 1,
+      priority = 100,
+    })
   end,
 }
