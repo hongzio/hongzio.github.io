2accd3bc8fb3413ecb75199480003c3b91ef2fcb
hongzio <hongzio@hongzios-alphanonce-macbook-pro.local>
2024-10-14 19:14:58 +0900

    feat(neo-tree): add keybindings for NeoTree explorer
    - Added keybinding <leader>e to open NeoTree at the root directory.
    - Added keybinding <leader>E to open NeoTree at the current working
    directory.
    - Both keybindings include descriptions and remap options.

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,24 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("neo-tree.command").execute({ toggle = false, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+        remap = true,
+      },
+      {
+        "<leader>E",
+        function()
+          require("neo-tree.command").execute({ toggle = false, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+        remap = true,
+      },
+    },
     opts = {
       filesystem = {
         window = {
