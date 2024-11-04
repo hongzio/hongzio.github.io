fb55c70601d0c653e7ddfa74660f4ae2ff78cd68
hongzio <hongzio@hongzios-alphanonce-macbook-pro.local>
2024-11-04 10:48:03 +0900

    feat: update luanchpad.nvim and add keymap for debug

--- lazyvim/lua/plugins/launchpad.lua
+++ lazyvim/lua/plugins/launchpad.lua
@@ -10,5 +10,6 @@ return {
     {"<leader>R", function() end, desc="+Launchpad"},
     {"<leader>RC", function() require("launchpad").create_config("run") end, desc="Create run configuration", mode = {"n"}},
     {"<leader>RR", function() require("launchpad").show_configs("run") end, desc="Run configurations", mode = {"n"}},
+    {"<leader>RD", function() require("launchpad").show_configs("debug") end, desc="Debug configurations", mode = {"n"}},
   },
 }
