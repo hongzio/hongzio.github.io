6e3f8798a0146fedc0610fd4daf45c482a3c539b
hongzio <hongzio@user.noreply.github.com>
2024-11-29 04:53:23 +0000

    feat(nvim): add visidf plugin

---
+++ lazyvim/lua/plugins/visidf.lua
@@ -0,0 +1,20 @@
+return {
+  "hongzio/visidf.nvim",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+    "folke/snacks.nvim",
+  },
+  opts = {
+    vdpath = "vd",
+  },
+  keys = {
+    {
+      "<leader>dv",
+      function()
+        require("visidf").run()
+      end,
+      desc = "visualize dataframe",
+      mode = { "v" },
+    },
+  },
+}
