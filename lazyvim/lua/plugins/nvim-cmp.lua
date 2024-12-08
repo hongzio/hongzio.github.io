0afabe6b1657e147ec6eb172475a4390e80d7426
hongzio <hongzio@user.noreply.github.com>
2024-12-08 17:15:32 +0000

    feat(nvim-cmp): add lspkind and key mappings
    - Added `onsails/lspkind.nvim` as a dependency for `nvim-cmp`.
    - Introduced new key mappings for scrolling docs and selecting items.
    - Configured `lspkind` for better symbol formatting in completion menu.
    - Updated `lazy-lock.json` to reflect the new commits for dependencies.

--- lazyvim/lua/plugins/nvim-cmp.lua
+++ lazyvim/lua/plugins/nvim-cmp.lua
@@ -1,16 +1,20 @@
 return {
   "hrsh7th/nvim-cmp",
-  dependencies = { "hrsh7th/cmp-emoji" },
+  dependencies = { "hrsh7th/cmp-emoji", "onsails/lspkind.nvim" },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local cmp = require("cmp")
     local cmp_window = require("cmp.config.window")
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<Tab>"] = cmp.mapping.confirm({ select = true }),
       ["<CR>"] = cmp.mapping(function(fallback)
         cmp.abort()
         fallback()
       end, { "i", "s" }),
+      ["<C-k>"] = cmp.mapping.select_prev_item(),
+      ["<C-j>"] = cmp.mapping.select_next_item(),
     })
 
     table.insert(opts.sources, { name = "emoji" })
@@ -28,5 +32,14 @@ return {
       completion = cmp_window.bordered(),
       documentation = cmp_window.bordered(),
     }
+    local lspkind = require("lspkind")
+    opts.formatting.format = lspkind.cmp_format({
+      mode = "symbol_text",
+      max_width = 50,
+      symbol_map = {
+        Copilot = "",
+        Codeium = "",
+      },
+    })
   end,
 }
