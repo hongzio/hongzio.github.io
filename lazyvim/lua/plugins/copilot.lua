31d87c9d8ae6eb60b8091cae9b14bea28b9d78da
hongzio <hongzio@user.noreply.github.com>
2024-12-08 18:02:57 +0000

    feat(copilot): support completion on empty line

--- lazyvim/lua/plugins/copilot.lua
+++ lazyvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   build = ":Copilot auth",
-  event = "InsertEnter",
+  event = {"InsertEnter", "CursorHoldI"},
   opts = {
     suggestion = {
       enabled = false,
