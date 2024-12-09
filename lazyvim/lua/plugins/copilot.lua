1f097a6bb93a9d47eec8771bb8664c68fb43dada
hongzio <11085613+hongzio@users.noreply.github.com>
2024-12-09 15:44:54 +0900

    feat(copilot): revert empty line completion
    This reverts commit 31d87c9d8ae6eb60b8091cae9b14bea28b9d78da.

--- lazyvim/lua/plugins/copilot.lua
+++ lazyvim/lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
   build = ":Copilot auth",
-  event = {"InsertEnter", "CursorHoldI"},
+  event = "InsertEnter",
   opts = {
     suggestion = {
       enabled = false,
