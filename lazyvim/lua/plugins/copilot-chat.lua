26d4797f87787caca2b35b116b470cc76e660b66
hongzio <hongzio@user.noreply.github.com>
2024-12-13 07:34:01 +0000

    feat: add commit prompt to copilot-chat plugin

--- lazyvim/lua/plugins/copilot-chat.lua
+++ lazyvim/lua/plugins/copilot-chat.lua
@@ -10,6 +10,18 @@ return {
     opts = {
       debug = false, -- Enable debugging
       -- See Configuration section for rest
+      prompts = {
+        Commit = {
+          prompt = "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.",
+          selection = function()
+            return {
+              filetype = "text",
+              filename = "unknown",
+              content = "",
+            }
+          end,
+        },
+      },
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
